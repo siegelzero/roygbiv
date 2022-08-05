b900facf0a904a1b0b40061c5b3f34c293e5e6b4
Kenneth Brown <siegel.zero@gmail.com>
2022-08-05 14:44:29 -0700

    Added comments

--- src/roygbiv/graph.nim
+++ src/roygbiv/graph.nim
@@ -1,24 +1,35 @@
 type
   Vertex* = int
   Graph* = object
+    # n is the number of vertices
     n*: int
+
+    # neighbors[u] contains the vertices v that share an edge with u
     neighbors*: seq[seq[Vertex]]
+
   
 iterator vertices*(graph: Graph): Vertex =
+  # Iterator over the vertices of the graph
   for u in 0..<graph.n:
     yield u
 
+
 iterator edges*(graph: Graph): (Vertex, Vertex) =
+  # Iterator over the edges (u, v) of the graph standardized with u < v.
   for u in graph.vertices:
     for v in graph.neighbors[u]:
       if u < v:
         yield (u, v)
 
+
 func initGraph*(n: int): Graph =
+  # Returns new Graph on n vertices
   result.n = n
   result.neighbors = newSeq[seq[int]](n)
 
+
 func addEdge*(graph: var Graph, u, v: Vertex) =
+  # Adds edge (u, v) to the graph
   graph.neighbors[u].add(v)
   graph.neighbors[v].add(u)
 
